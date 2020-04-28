190ccad4cee3565684b31147809ef9ce94db0b5e
Aissen <Aissen@outlook.com>
2020-04-28 23:28:16 +0800

    create test file

---
+++ src/test/java/com/jiker/keju/TaximeterWrapperTest.java
@@ -0,0 +1,11 @@
+package com.jiker.keju;
+
+import org.junit.jupiter.api.Test;
+
+public class TaximeterWrapperTest {
+
+    @Test
+    public void should_show_price_given_only_paying_starting_pare(){
+        
+    }
+}
